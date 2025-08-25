08fe8d4e31a19bf2dfb066cef7c82d85cfc1bf1e
Rafaella Amorim <rafaellawamorim@gmail.com>
2025-08-25 14:48:46 -0300

    aula13 ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Saudacao from './components/Saudacao'
-import Rodape from './components/Rodape'
-import Alerta from './components/Alerta'
-import EmailImput from './components/EmailInput'
-import EmailField from './components/EmailField'
-import PasswordField from './components/PasswordField'
-import LoginButton from './components/LoginButton'
-import Perfil from './components/Perfil'
-import Produto from './components/Produto'
-import Container from '../Container'
-import Contador from './components/Contador'
-import ModoNoturno from './components/ModoNoturno'
-import Formulario from './components/Formulario'
-import FiltroProdutos from './components/FiltroProdutos'
-import Carrinho from './components/Carrinho'
-import Faq from './components/Faq'
-import ListaNomes from './components/ListaNomes'
 
-
+import { Routes, Route, Link } from 'react-router-dom';
+function HomePage() { return <h1>Página Inicial</h1>; }
+function ContatoPage() { return <h1>Sobre Nós</h1>; }
+ 
 function App() {
-const nome = "Rafaella";
-const anoNascimento = 1976;
-      function calculaIdade() {
-      return 2025 - anoNascimento;
-} 
-return (  
-          <div>
-            <h1> Olá, {nome}!</h1>;
-            <h1> Você tem {calculaIdade()} anos</h1>;
-            
-            	<Saudacao />
-		      <Produto nome="caderno" preco={3.5} emEstoque={true}/>
-                  <Produto nome="Mouse" preco={80.00} emEstoque={false} />
-                  <Produto nome="Laptop" preco={4500.50} emEstoque={true} />
-                  <Container>
-            <h1>Título Principal</h1>
-            <p>Todo este conteúdo está dentro do container.</p>
-            <p>Rafaella está testanddddoooo.</p>
-             </Container>
-            <Contador/>
-            <ModoNoturno/>
-            <Formulario/>
-            <FiltroProdutos/>
-            <Carrinho/>
-            <Faq/>
-            <ListaNomes/>
-
-
-
-          </div>
-
-)    
+  return (
+    <div>
+      <nav>
+        <Link to="/">Início</Link> | <Link to="/Contato">ContatoPage</Link>
+      </nav>
+      <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Contato" element={<ContatoPage />} />
+      </Routes>
+    </div>
+  );
 }
+
+
+
+
+
 
 export default App
---
+++ src/PAGES/HomePage.jsx
@@ -0,0 +1,3 @@
+function HomePage() { return <h1>Bem-vindo à Página Inicial!</h1>; }
+export default HomePage;
+ 
